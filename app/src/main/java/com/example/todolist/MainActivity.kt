cd5f048d6bd64ebad9565a0635508cd9f260da77
ferroa <00300722@uca.edu.sv>
2024-05-04 05:27:17 -0600

    estructura principal

---
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.todolist
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.todolist.ui.theme.ToDoListTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ToDoListTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HomeScreen()
+                }
+            }
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreen(){
+    Scaffold (
+        bottomBar = {
+
+        }
+    ){
+        padding ->
+
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+        ){
+            //MiguelList()
+            //FranList()
+
+        }
+
+
+    }
+}
+
+
+
+
